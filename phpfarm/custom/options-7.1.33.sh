271673048a2c7d92e58e35cb11b9d486d4ce8f93
Christian Boulanger <info@bibliograph.org>
2021-07-27 23:46:53 +0200

    Update configure options

--- phpfarm/custom/options-7.1.33.sh
+++ phpfarm/custom/options-7.1.33.sh
@@ -1,26 +1,26 @@
 configoptions="$configoptions \
     --enable-gd-native-ttf \
     --enable-intl \
-    --with-ldap \
-    --with-ldap-sasl \
     --with-bz2 \
     --with-config-file-scan-dir=/var/www/.php \
     --with-curl \
+    --with-freetype-dir=/usr/include/freetype2/ \
     --with-gd \
     --with-jpeg-dir=/usr/lib \
     --with-kerberos \
+    --with-ldap \
+    --with-ldap-sasl \
     --with-libdir=$LIBPATH \
     --with-mcrypt \
     --with-mhash \
-    --with-openssl \
     --with-mysql \
     --with-mysqli \
+    --with-openssl \
     --with-pdo-mysql \
     --with-pdo-pgsql \
-    --with-png-dir=/usr/lib \
     --with-pgsql \
+    --with-png-dir=/usr/lib \
     --with-xsl=/usr \
-    --with-freetype-dir=/usr/include/freetype2/ \
 "
 
 echo "--- loaded custom/options-7.2.x.sh ----------"
--- phpfarm/custom/options-8.0.0.sh
+++ phpfarm/custom/options-8.0.0.sh
@@ -1,24 +1,28 @@
-# custom options file for phpfarm
-
 configoptions="\
+    --enable-calendar \
+    --enable-embedded-mysqli \
+    --enable-gd \
     --enable-gd-native-ttf \
     --enable-intl \
-    --with-ldap \
-    --with-ldap-sasl \
+    --enable-mbstring \
     --with-bz2 \
     --with-config-file-scan-dir=/var/www/.php \
     --with-curl \
+    --with-gettext \
     --with-kerberos \
+    --with-ldap \
+    --with-ldap-sasl \
     --with-libdir=$LIBPATH \
     --with-mhash \
-    --with-openssl \
     --with-mysql \
     --with-mysqli \
+    --with-openssl \
     --with-pdo-mysql \
     --with-pdo-pgsql \
     --with-pgsql \
     --with-xsl=/usr \
-    --enable-embedded-mysqli"
+    --with-zip \
+"
 
 echo "--- loaded custom/options-8.0.0.sh ---"
 echo $configoptions
